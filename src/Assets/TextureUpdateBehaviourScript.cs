178fbdb05d4c32cb78a976a2c8a858827eb3c685
takashi imagire <imagire@gmail.com>
2019-12-06 11:32:33 +0900

    add files

---
+++ src/Assets/TextureUpdateBehaviourScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextureUpdateBehaviourScript : MonoBehaviour
+{
+    [SerializeField] CustomRenderTexture _textureSobel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _textureSobel.Update(1);
+    }
+}
